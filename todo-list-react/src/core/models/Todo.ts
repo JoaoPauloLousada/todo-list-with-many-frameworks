0e54bb5598033c259f46f3718b1ca17efbbdbaf1
Joao Lousada <joao.lousada.ext@makeo.com>
2024-06-15 18:24:52 +0200

    [code] Update core

--- todo-list-react/src/core/models/Todo.ts
+++ todo-list-react/src/core/models/Todo.ts
@@ -2,7 +2,7 @@ export type TodoInput = Partial<Omit<Todo, "text">> & Pick<Todo, "text">;
 export type TodoUpdate = Partial<Omit<Todo, "id">>;
 
 export class Todo {
-  id: string = "";
+  id: string = this.createId();
   text: string = "";
   datetime?: Date = undefined;
   completed: boolean = false;
@@ -10,8 +10,7 @@ export class Todo {
   private constructor(data: TodoInput) {
     Object.assign(this, {
       ...data,
-      completed: data.completed ?? false,
-      id: this.createId(),
+      datetime: data.datetime ? new Date(data.datetime) : undefined,
     });
   }
 
--- todo-list-react/src/core/usecases/GetTodoList.usecase.ts
+++ todo-list-react/src/core/usecases/GetTodoList.usecase.ts
@@ -1,6 +1,7 @@
 import { TodoList } from "../models/TodoList";
 import { IGetTodoList } from "../ports/Http";
 import { IUseCase } from "./IUseCase";
+import { Todo } from "../models/Todo";
 
 export class GetTodoList implements IUseCase<undefined, TodoList> {
   constructor(private todoHttp: IGetTodoList) {}
@@ -16,7 +17,7 @@ export class GetTodoList implements IUseCase<undefined, TodoList> {
 
     if (response.data) {
       next?.({
-        data: TodoList.create(response.data),
+        data: TodoList.create(response.data.map((t) => Todo.create(t))),
         error: null,
       });
       return;
