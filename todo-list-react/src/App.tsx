1cb715a3a37d31d2bc436ae1fe8a2ac52e03a6ea
Joao Lousada <joao.lousada.ext@makeo.com>
2024-06-15 16:10:44 +0200

    [react] Style App container

--- todo-list-react/src/App.tsx
+++ todo-list-react/src/App.tsx
@@ -1,10 +1,12 @@
+import { TodoAdd } from "./components/TodoAdd";
 import { TodoListComponent } from "./components/TodoList";
 
 function App() {
   return (
-    <>
+    <div className="w-full sm:w-[768px] mx-auto mt-4 p-4 border">
+      <TodoAdd />
       <TodoListComponent />
-    </>
+    </div>
   );
 }
 
