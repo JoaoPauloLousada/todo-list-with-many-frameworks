14dd72209f082772bd0c13d55626eab91e4afd49
Joao Lousada <joao.lousada.ext@makeo.com>
2024-06-15 18:23:38 +0200

    [react] Update dependencies

--- todo-list-react/src/dependencies.ts
+++ todo-list-react/src/dependencies.ts
@@ -2,8 +2,14 @@ import { requestHandler } from "./core/utils/RequestHandler";
 import { TodoHttpService } from "./core/services/TodoHttp.service";
 import { FetchAdapter } from "./core/adapters/Fetch.adapter";
 import { GetTodoList } from "./core/usecases/GetTodoList.usecase";
+import { CreateTodo } from "./core/usecases/CreateTodo.usecase";
+import { DeleteTodo } from "./core/usecases/DeleteTodo.usecase";
+import { UpdateTodo } from "./core/usecases/UpdateTodo.usecase";
 
 const fetchAdapter = new FetchAdapter();
 const todoHttpService = new TodoHttpService(fetchAdapter, requestHandler);
 
 export const getTodoListUseCase = new GetTodoList(todoHttpService);
+export const createTodoUseCase = new CreateTodo(todoHttpService);
+export const deleteTodoUseCase = new DeleteTodo(todoHttpService);
+export const updateTodoUseCase = new UpdateTodo(todoHttpService);
